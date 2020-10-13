43012c3708ab4d272d932907a4880495fd7c56b4
Mishgen1 <32348690+Mishgen1@users.noreply.github.com>
2020-10-13 22:24:17 +0800

    init: start towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 
 // You should implement your task here.
 
+
 module.exports = function towelSort (matrix) {
-  return [];
+  if(!Array.isArray(matrix)) {return []}
+  mas = [];
+  temp = [];
+  otv = [];
+  let i =1;
+  matrix.forEach(e=>{
+    i++
+    e.forEach(a => {
+      console.log(i)
+      temp.push(a)
+      if(i%2 === 0){
+        temp.sort(function(a,b){
+          return a-b
+        })
+      }
+      else{
+        temp.sort(function(a,b){
+          return b-a
+        })
+      }
+    })
+    mas.push(temp)
+    temp = []
+})
+
+mas.forEach(e =>{
+ e.forEach(l =>{
+   otv.push(l)
+ })
+})
+return otv;
 }
